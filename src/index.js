0649e013fed235a3da658c8762cd872a46533217
Amy Y <ctrlalternativedelete@gmail.com>
2021-03-02 21:50:29 +1100

    moved polyfillconverter to seperate file

--- src/index.js
+++ src/index.js
@@ -81,23 +81,10 @@ newAddedValues.forEach(element => {
 //convert back to xml
 FileHandler.saveFile(newoutputname, FileHandler.obj2xml(oldObject))
 
-// //Polyfill Converter
-// let defaultObject = {
-//     "Ableton": {
-//       "$": {
-//         "MajorVersion": "5",
-//         "MinorVersion": "11.0_432",
-//         "SchemaChangeCount": "3",
-//         "Creator": "Ableton Live 11.0.1d1",
-//         "Revision": ""
-//       }
-//     }
-// }
-// let newObjectofValues = newAddedValues.reduce((result, current) => {
-//     return Object.assign(result, current);
-// }, {});
-// defaultObject.Ableton['Theme'] = [newObjectofValues]
-// FileHandler.saveFile('./src/backupJsons/00LightPolyfill.json', JSON.stringify(defaultObject, null, 2))
+//Polyfill Converter:
+// const PolyFillHandler = new (require('./modules/polyfill/polyfillhandler.module'))
+// FileHandler.saveFile('./src/modules/polyfill/backupJsons/00LightPolyfill.json', JSON.stringify(PolyFillHandler.polyfillConverter(newAddedValues), null, 2))
+
 
 
 
