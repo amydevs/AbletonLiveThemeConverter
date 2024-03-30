5756e9804d7717c30856dd06b3bd221848cb41c1
Amy Yan <ayanamydev@gmail.com>
2024-03-30 16:41:10 +1100

    fix: shell.nix

--- shell.nix
+++ shell.nix
@@ -6,17 +6,19 @@ mkShell {
     gitAndTools.gh
     # Rust
     rustc
+    rustc-wasm32
     cargo
     gcc
+    llvmPackages.bintools
     rustfmt
     clippy
-    # Fly
-    flyctl
+    wasm-pack
   ];
   # Don't set rpath for native addons
   NIX_DONT_SET_RPATH = true;
   NIX_NO_SELF_RPATH = true;
   RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+  CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
   shellHook = ''
 
   '';
