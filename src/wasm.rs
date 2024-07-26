31d4f8f9431b02934fa5f7b7a753c2a47c8b7809
Amy Yan <ayanamydev@gmail.com>
2024-07-27 04:42:30 +1000

    fix: added convert method to wasm

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,6 +1,7 @@
 use crate::util::{
     get_live_version as get_live_version_internal,
     parse_ask as parse_ask_internal,
+    convert as convert_internal,
     convert_ask as convert_ask_internal,
     LiveVersion,
     LiveWrapper,
@@ -15,6 +16,11 @@ pub fn get_live_version(xml: &str) -> Option<LiveVersion> {
 #[wasm_bindgen]
 pub fn parse_ask(xml: &str, version: LiveVersion) -> Result<LiveWrapper, JsError> {
     Ok(parse_ask_internal(xml, version)?)
+}
+
+#[wasm_bindgen]
+pub fn convert(from: LiveWrapper, to_version: LiveVersion) -> LiveWrapper {
+    convert_internal(from, to_version)
 }
 
 #[wasm_bindgen]
